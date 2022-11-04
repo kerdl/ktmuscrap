04dff1557810fe1ec01c66ba06d41ec3d9d74556
kerdl <clayshowtv@gmail.com>
2022-11-04 20:31:03 +0300

    f

--- src/api/schedule/daily.rs
+++ src/api/schedule/daily.rs
@@ -1,16 +1,24 @@
 use actix_web::{get, delete, post, Responder, web};
 
-use crate::{RAW_SCHEDULE, parse};
+use crate::{RAW_SCHEDULE, parse, api::Response};
+use super::error_response;
 
 
 #[post("/schedule/daily/convert")]
 async fn convert() -> impl Responder {
-    parse::daily(
+    let parsing_result = parse::daily(
         RAW_SCHEDULE.ft_daily.clone(), 
         RAW_SCHEDULE.r_weekly.clone()
-    ).await.unwrap();
+    ).await;
 
-    ""
+    if parsing_result.is_err() {
+        return error_response(
+            parsing_result.unwrap_err()
+        ).await;
+    }
+    let parsed = parsing_result.unwrap();
+
+    Response::from_schedule(parsed).to_json()
 }
 
 #[get("/schedule/daily")]
--- src/api/schedule/mod.rs
+++ src/api/schedule/mod.rs
@@ -2,9 +2,9 @@ pub mod raw;
 pub mod weekly;
 pub mod daily;
 
-use actix_web::Responder;
+use actix_web::web;
 use tokio::sync::RwLock;
-use std::{future::Future, sync::Arc};
+use std::{future::Future, sync::Arc, error::Error};
 
 use crate::{
     api::{self, error::base::ToApiError, Response, ToResponse}, 
@@ -13,42 +13,28 @@ use crate::{
 };
 
 
-pub async fn generic_parse<Parser, FutParsed>(
-    parser: Parser,
-    schedule: Arc<RwLock<Zip>>
-) -> impl Responder
-where
-    Parser: FnOnce(Arc<RwLock<Zip>>) -> FutParsed,
-    FutParsed: Future<Output = SyncResult<()>>
-{
-    let parsed = parser(schedule).await;
+pub async fn error_response(
+    err: Box<dyn Error + Send + Sync>
+) -> web::Json<Response> {
+    match err {
+        // io error from tokio
+        tokio_err if err.is::<tokio::io::Error>() => {
 
-    if parsed.is_err() {
-        let err = parsed.unwrap_err();
+            let err: &tokio::io::Error = {
+                tokio_err.downcast_ref().unwrap()
+            };
 
-        match err {
-            // io error from tokio
-            tokio_err if err.is::<tokio::io::Error>() => {
-
-                let err: &tokio::io::Error = {
-                    tokio_err.downcast_ref().unwrap()
-                };
-
-                return api::error::IoError::new(format!("{}", err))
-                    .to_api_error()
-                    .to_response()
-                    .to_json()
-            }
-            // anything else, retard?
-            _ => {
-                return api::error::Unknown::new(err.to_string())
-                    .to_api_error()
-                    .to_response()
-                    .to_json()
-            }
+            return api::error::IoError::new(format!("{}", err))
+                .to_api_error()
+                .to_response()
+                .to_json()
         }
-
+        // anything else, retard?
+        _ => {
+            return api::error::Unknown::new(err.to_string())
+                .to_api_error()
+                .to_response()
+                .to_json()
+        }
     }
-
-    Response::ok().to_json()
 }
