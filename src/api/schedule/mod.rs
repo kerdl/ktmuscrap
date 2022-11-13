7a0141b3e91cfc2fe4bd0ebb2f58f090dc00d011
kerdl <clayshowtv@gmail.com>
2022-11-13 20:16:01 +0300

    um acktualy

--- src/api/schedule/mod.rs
+++ src/api/schedule/mod.rs
@@ -81,10 +81,6 @@ impl Actor for UpdatesWs {
             while notify_rx.changed().await.is_ok() {
                 let notify = notify_rx.borrow();
 
-                if notify.daily.is_none() && notify.weekly.is_none() {
-                    continue;
-                }
-
                 match &notify.invoker {
                     Invoker::Auto => {},
                     Invoker::Manually(invoker) => {
